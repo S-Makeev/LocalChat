1cd2412e6ed03658b48763c0d2189e39a56dde66
Stanislav Petrov <sv.petrov@kodep.pro>
2020-11-04 18:05:19 +0200

    + IConsole

---
+++ LocalChat/DefaultConsole.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LocalChat
+{
+    public class DefaultConsole : IConsole
+    {
+        public ConsoleColor ForegroundColor { get => Console.ForegroundColor; set => Console.ForegroundColor = value; }
+
+        public string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+
+        public void ResetColor()
+        {
+            Console.ResetColor();
+        }
+
+        public void Write(string value)
+        {
+            Console.Write(value);
+        }
+
+        public void WriteLine(string value)
+        {
+            Write(value + "\n");
+        }
+    }
+}
